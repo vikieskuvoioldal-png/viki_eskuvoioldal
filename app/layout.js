e732004a8048ca277d13a9310552680c5e598a8d
Kovács Bálint <230157220+vikieskuvoioldal-png@users.noreply.github.com>
2025-09-03 15:00:53 +0200

    alapok

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,28 @@
-import { cormorant, playfair, lora, montserrat, vibes, dancing } from '../lib/fonts'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-
 
 export const metadata = {
-  title: 'LACE Esküvők',
-  description: 'Esküvői meghívók és grafikai tervezés',
+  title: 'Anna & Balázs Esküvője',
+  description: 'Szeretettel meghívunk az esküvőnkre!',
   icons: {
     icon: [
       { url: '/images/favicon.ico' },
-      { url: '/images/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/images/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      // Itt megadhatsz további favicon méreteket, ha vannak
     ],
     apple: '/images/apple-touch-icon.png',
-    android: [
-      { url: '/images/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/images/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-    ],
-    shortcut: '/images/favicon.ico',
   },
 }
 
-
-const fontVariables = `${montserrat.variable} ${cormorant.variable} ${dancing.variable} ${playfair.variable} ${lora.variable} ${vibes.variable}`;
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat'
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="hu" className={fontVariables}>
-      <body className="bg-white text-dark-text">
+    <html lang="hu" className={montserrat.variable}>
+      <body className="bg-white text-gray-800">
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'bjjjpntdcydixfgurtus.supabase.co'],
   },
-  async headers() {
-    return [
-      {
-        source: "/demo/:path*", // csak a /demo alatti route-okra érvényes
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN", // ez engedi iframe-ben a betöltést ugyanazon originről
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
